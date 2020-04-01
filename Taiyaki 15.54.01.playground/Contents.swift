259b4743bd423cc70745b049100cc09fe8554b8a
奥野豪太 <okuno@freemake.co.jp>
2020-04-01 16:41:49 +0900

    参考書p142「クラスを作成する」の内容を学習いたしましたので、Githubの使い方の学習も兼ねてアップいたします。

---
+++ Taiyaki 15.54.01.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var str = "Hello, playground"
+
+class Taiyaki { //Taiyakiクラスを宣言
+    var nakami = "あんこ" //nakamiプロパティを宣言
+    func sayNakami() { //sayNakamiメソッドを宣言
+        print("中身は" + nakami + "です。")
+    }
+}
+
+var taiyaki = Taiyaki()
+taiyaki.nakami = "クリーム"
+taiyaki.sayNakami()
+
+//参考書p142「クラスを作成する」
