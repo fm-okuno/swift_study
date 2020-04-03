444806abe51bd37ad245153b3b7638473112a1c2
gohta <okuno@freemake.co.jp>
2020-04-03 10:26:21 +0900

    VieController.swift 　1.@IBOutletと@IBActionにPrivateを付与 　2.sayHelloメソッドをactionTextChangeButtonメソッドに変更 　3.引数labelTextの中身をlabelインスタンスのtestプロパティに代入するshowMessageメソッドを作成、actionTextChangeButton内で呼び出すようにした
    Main.storybord
    　1.sayHelloメソッドの変更に伴う再接続作業実施

--- Hello/Hello/ViewController.swift
+++ Hello/Hello/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit //UIKitフレームワークを読み込み
 
 class ViewController: UIViewController { //親クラス「UIViewControllerを」を継承した子クラス「ViewController」
+    
+    private func showMessage(labelText:String) { //引数labelTextの内容をlabelに表示するshowMessageメソッド
+        label.text = labelText
+    }
 
-    @IBOutlet weak var label: UILabel! //labelと接続
-    @IBAction func sayHello() { //buttonと接続されたsayHelloメソッド
-        label.text = "こんにちは" //button押下でlabelの文字を変更
+    @IBOutlet private weak var label: UILabel! //labelと接続
+    @IBAction private func actionTextChangeButton() { //buttonと接続されたactionTextChangeButtonメソッド
+       showMessage(labelText: "こんにちは")
     }
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,4 +25,3 @@ class ViewController: UIViewController { //親クラス「UIViewControllerを」
 
 
 }
-
