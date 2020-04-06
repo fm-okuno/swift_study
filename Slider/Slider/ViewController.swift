6a36656859bc335cc5981dac65326f2219415c81
gohta <okuno@freemake.co.jp>
2020-04-06 13:46:26 +0900

    現在のSliderの値をLabelに表示するSliderアプリを作成します。 プロジェクトを作成しました。

---
+++ Slider/Slider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Slider
+//
+//  Created by 開発アカウント on 2020/04/06.
+//  Copyright © 2020 開発アカウント. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
