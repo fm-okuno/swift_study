84c14ec1e8e294b462e6918b49367d5caf150eee
gohta <okuno@freemake.co.jp>
2020-04-06 14:05:23 +0900

    参考書p209~p215のSliderの値をLabelに表示させるアプリを作成。
    Main.storybord
    　・ラベルとスライダーを配置し、ViewController.swiftと接続。
    
    ViewController.swift
    　・Sliderの値を引数senderを使って取得し、label.textに代入して表示。

--- Slider/Slider/ViewController.swift
+++ Slider/Slider/ViewController.swift
@@ -5,6 +5,7 @@
 //  Created by 開発アカウント on 2020/04/06.
 //  Copyright © 2020 開発アカウント. All rights reserved.
 //
+//　Sliderの値を取得し、Labelに表示させるアプリ
 
 import UIKit
 
@@ -15,6 +16,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet private weak var label: UILabel!
+    @IBAction private func showValue(_ sender: UISlider) { //Sliderの現在の値をsenderを使って取得し、label.textに代入して表示
+        label.text = "\(sender.value)"
+    }
+    
 }
 
